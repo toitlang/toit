99922db4f8813de56ae17c43ab877844fc6b11cc
Dusten Sobotta <dusten@hey.com>
2021-12-06 02:24:02 -0800

    fixed arm32.cmake toolchain

--- toolchains/arm32.cmake
+++ toolchains/arm32.cmake
@@ -22,13 +22,13 @@ set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -x assembler-with-cpp" CACHE STRING "asm
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "c++ flags")
 
-set(CMAKE_C_FLAGS_DEBUG "-m32 -Og -g -rdynamic -fdiagnostics-color" CACHE STRING "c Debug flags")
-set(CMAKE_C_FLAGS_RELEASE "-m32 -Os" CACHE STRING "c Release flags")
+set(CMAKE_C_FLAGS_DEBUG "-Og -g -rdynamic -fdiagnostics-color" CACHE STRING "c Debug flags")
+set(CMAKE_C_FLAGS_RELEASE "-Os" CACHE STRING "c Release flags")
 set(CMAKE_C_FLAGS_ASAN "-O1 -fsanitize=address -fno-omit-frame-pointer -g" CACHE STRING "c Asan flags")
 set(CMAKE_C_FLAGS_PROF "-Os -DPROF -pg" CACHE STRING "c Prof flags")
 
-set(CMAKE_CXX_FLAGS_DEBUG "-m32 -Og -ggdb3 -rdynamic -fdiagnostics-color $ENV{LOCAL_CXXFLAGS}" CACHE STRING "c++ Debug flags")
-set(CMAKE_CXX_FLAGS_RELEASE "-m32 -Os $ENV{LOCAL_CXXFLAGS}" CACHE STRING "c++ Release flags")
+set(CMAKE_CXX_FLAGS_DEBUG "-Og -ggdb3 -rdynamic -fdiagnostics-color $ENV{LOCAL_CXXFLAGS}" CACHE STRING "c++ Debug flags")
+set(CMAKE_CXX_FLAGS_RELEASE "-Os $ENV{LOCAL_CXXFLAGS}" CACHE STRING "c++ Release flags")
 set(CMAKE_CXX_FLAGS_ASAN "-O1 -fsanitize=address -fno-omit-frame-pointer -g" CACHE STRING "c++ Asan flags")
 set(CMAKE_CXX_FLAGS_PROF "-Os -DPROF -pg" CACHE STRING "c++ Prof flags")
 
