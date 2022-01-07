bb7b7daca64b0cfd5920ecc1387fcbaad8eba360
Florian Loitsch <florian@toit.io>
2022-01-07 11:33:44 +0100

    Change paths from / to \ on Windows for gold files. (#268)

--- tests/tools/normalize_gold.cmake
+++ tests/tools/normalize_gold.cmake
@@ -1,9 +1,29 @@
 # Copyright (C) 2021 Toitware ApS. All rights reserved.
 
 function(NORMALIZE_GOLD INPUT PREFIX_TO_REMOVE GIT_VERSION OUTPUT)
+  file(TO_NATIVE_PATH "${PREFIX_TO_REMOVE}" PREFIX_TO_REMOVE)
   string(REPLACE "${PREFIX_TO_REMOVE}" "<...>" RESULT "${INPUT}")
   if (NOT "${GIT_VERSION}" STREQUAL "")
     string(REPLACE "${GIT_VERSION}" "<GIT_VERSION>" RESULT "${RESULT}")
   endif()
-  set(${OUTPUT} ${RESULT} PARENT_SCOPE)
+  set(${OUTPUT} "${RESULT}" PARENT_SCOPE)
+endfunction()
+
+function(LOCALIZE_GOLD INPUT OUTPUT)
+  message("${CMAKE_SYSTEM_NAME}")
+  if (NOT "${CMAKE_SYSTEM_NAME}" STREQUAL "Windows")
+    set(${OUTPUT} "${INPUT}" PARENT_SCOPE)
+    return()
+  endif()
+
+  # Find all potential paths and replace them.
+  string(REGEX MATCHALL "(/?tests[a-zA-Z_0-9/]+)|([a-zA-Z_0-9/]+[.]toit)" PATHS "${INPUT}")
+  list(REMOVE_DUPLICATES PATHS)
+  set(RESULT "${INPUT}")
+  foreach(PATH ${PATHS})
+    string(REPLACE "/" "\\" REPLACEMENT "${PATH}")
+    string(REPLACE "${PATH}" "${REPLACEMENT}" RESULT "${RESULT}")
+  endforeach()
+
+  set(${OUTPUT} "${RESULT}" PARENT_SCOPE)
 endfunction()
