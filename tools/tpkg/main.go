3b18c30ce4024d1ed81af9ad28d8c3db3acd691d
Florian Loitsch <florian@loitsch.com>
2022-01-12 17:14:16 +0100

    Update copyright year.

--- tools/tpkg/main.go
+++ tools/tpkg/main.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2021 Toitware ApS.
+// Copyright (C) 2022 Toitware ApS.
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
