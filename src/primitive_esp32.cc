4258c8bad17ec5c39b9510a491d82c32a501660f
Lau Skorstengaard <lau@toit.io>
2022-01-28 13:38:26 +0100

    Remove newline

--- src/primitive_esp32.cc
+++ src/primitive_esp32.cc
@@ -138,7 +138,6 @@ PRIMITIVE(get_mac_address) {
 
 PRIMITIVE(rtc_user_bytes) {
   uint8* rtc_memory = RtcMemory::user_data_address();
-
   Error* error = null;
   ByteArray* result = process->object_heap()->allocate_external_byte_array(RtcMemory::RTC_USER_DATA_SIZE, rtc_memory, false, false);
   if (result == null) return error;
