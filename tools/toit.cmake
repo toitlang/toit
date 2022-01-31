8bbe18bc1282a8d7a1867a2545d1559e2a9f56b6
Florian Loitsch <florian@toit.io>
2022-01-31 11:54:43 -0800

    Add support for toit-projects (#381)
    At the moment we just download the required packages.

--- tools/toit.cmake
+++ tools/toit.cmake
@@ -13,6 +13,34 @@
 # The license can be found in the file `LICENSE` in the top level
 # directory of this repository.
 
+# This file serves as normal cmake include, as well as a cmake-script, run with
+# `cmake -P`.
+# In the latter case the `EXECUTING_SCRIPT` variable is defined, and we only
+# process the command that we should execute.
+set(TOIT_DOWNLOAD_PACKAGE_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/toit.cmake")
+if (DEFINED EXECUTING_SCRIPT)
+  if ("${SCRIPT_COMMAND}" STREQUAL "install_packages")
+    if (NOT DEFINED TOIT_PROJECT)
+      message(FATAL_ERROR "Missing TOIT_PROJECT")
+    endif()
+    if (NOT DEFINED TOITPKG)
+      message(FATAL_ERROR "Missing TOITPKG")
+    endif()
+
+    if (EXISTS "${TOIT_PROJECT}/package.yaml" OR EXISTS "${TOIT_PROJECT}/package.lock")
+      execute_process(
+        COMMAND "${TOITPKG}" install "--project-root=${TOIT_PROJECT}"
+        COMMAND_ERROR_IS_FATAL ANY
+      )
+    endif()
+  else()
+    message(FATAL_ERROR "Unknown script command ${SCRIPT_COMMAND}")
+  endif()
+
+  # End the execution of this file.
+  return()
+endif()
+
 # Creates a custom command to build ${TARGET} with correct dependencies.
 function(ADD_TOIT_TARGET SOURCE TARGET DEP_FILE ENV)
   if (NOT DEFINED TOITC)
@@ -32,3 +60,28 @@ function(ADD_TOIT_TARGET SOURCE TARGET DEP_FILE ENV)
     COMMAND ${CMAKE_COMMAND} -E env ${ENV} ASAN_OPTIONS=detect_leaks=false "${TOITC}" --dependency-file "${DEP_FILE}" --dependency-format ninja -w "${TARGET}" "${SOURCE}"
   )
 endfunction(ADD_TOIT_TARGET)
+
+if (NOT TARGET download_packages)
+  add_custom_target(
+    download_packages
+  )
+endif()
+
+macro(toit_project NAME PATH)
+  # TODO(florian): find toitpkg in path.
+  if (NOT DEFINED TOITPKG)
+    message(FATAL_ERROR "Missing TOITPKG")
+  endif()
+
+  set(DOWNLOAD_TARGET_NAME "download-${NAME}-packages")
+  add_custom_target(
+    "${DOWNLOAD_TARGET_NAME}"
+    COMMAND "${CMAKE_COMMAND}"
+        -DEXECUTING_SCRIPT=true
+        -DSCRIPT_COMMAND=install_packages
+        "-DTOIT_PROJECT=${PATH}"
+        "-DTOITPKG=${TOITPKG}"
+        -P "${TOIT_DOWNLOAD_PACKAGE_SCRIPT}"
+  )
+  add_dependencies(download_packages "${DOWNLOAD_TARGET_NAME}")
+endmacro()
