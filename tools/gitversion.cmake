ea885bfdfd65c0005f3bf5717d446cc4885c7750
Florian Loitsch <florian@toit.io>
2022-02-01 08:03:52 -0800

    Improve git-version for release branches. (#397)

--- tools/gitversion.cmake
+++ tools/gitversion.cmake
@@ -60,25 +60,89 @@ function(compute_git_version VERSION)
   backtick(CURRENT_COMMIT_NO ${GIT_EXECUTABLE} rev-list --count HEAD "^${VERSION_TAG_COMMIT}")
   backtick(CURRENT_BRANCH ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD)
 
-  if ("${CURRENT_BRANCH}" MATCHES "^release-v[0-9]+\\.[0-9]$")
-    # Use next patch version when on a release branch.
-    MATH(EXPR patch "${patch}+1")
+  if ("${CURRENT_BRANCH}" MATCHES "^release-v([0-9]+)\\.([0-9]+)$")
+    set(branch_major ${CMAKE_MATCH_1})
+    set(branch_minor ${CMAKE_MATCH_2})
+    if (${branch_major} EQUAL ${major} AND ${branch_minor} EQUAL ${minor})
+      # There is already a release on this branch.
+      # Use next patch version.
+      MATH(EXPR patch "${patch}+1")
+    else()
+      # First release on this major.minor branch.
+      set(major ${branch_major})
+      set(minor ${branch_minor})
+      set(patch "0")
+    endif()
     set(${VERSION} "v${major}.${minor}.${patch}-pre.${CURRENT_COMMIT_NO}+${CURRENT_COMMIT_SHORT}" PARENT_SCOPE)
     return()
   endif()
 
-  if ("${CURRENT_BRANCH}" MATCHES "^release-v[0-9]+\\.[0-9]$")
+  # There might be a branch that hasn't released yet.
+  # As such the tag might not know about it.
+  backtick(ALL_BRANCHES ${GIT_EXECUTABLE} branch -a "--format=%(refname:short)")
+  # Split lines.
+  # Assumes there aren't any ';' in the branch names.
+  STRING(REGEX REPLACE "\n" ";" ALL_BRANCHES "${ALL_BRANCHES}")
+  # Find all remote release branches.
+  # We ignore the local ones.
+  set(RELEASE_BRANCHES ${ALL_BRANCHES})
+  list(FILTER RELEASE_BRANCHES INCLUDE REGEX "/release-v[0-9]+\\.[0-9]")
+  list(SORT RELEASE_BRANCHES COMPARE NATURAL ORDER DESCENDING)
+  list(LENGTH RELEASE_BRANCHES BRANCHES_LENGTH)
+  if (NOT ${BRANCHES_LENGTH} EQUAL 0)
+    list(GET RELEASE_BRANCHES 0 NEWEST_BRANCH)
+    string(REGEX MATCH "/release-v([0-9]+)\\.([0-9]+)" IGNORED "${NEWEST_BRANCH}")
+    set(branch_major ${CMAKE_MATCH_1})
+    set(branch_minor ${CMAKE_MATCH_2})
+  endif()
+
+  if ("${CURRENT_BRANCH}" STREQUAL master OR "${CURRENT_BRANCH}" STREQUAL main OR "${CURRENT_BRANCH}" STREQUAL HEAD)
     # Master branch: v0.5.0-pre.17+9a1fbdb29
+    # Use either the latest reachable tag, or the highest branch. Whichever is higher.
+    if ("${branch_major}.${branch_minor}" VERSION_GREATER "${major}.${minor}")
+      set(major ${branch_major})
+      set(minor ${branch_minor})
+    endif()
     MATH(EXPR minor "${minor}+1")
     set(${VERSION} "v${major}.${minor}.0-pre.${CURRENT_COMMIT_NO}+${CURRENT_COMMIT_SHORT}" PARENT_SCOPE)
     return()
   endif()
 
   # Other branch: v0.5.0-pre.17+branch-name.9a1fbd29
+  # If we are the child of a release-branch, use it, or a tag. Whichever is higher.
+  # If we aren't a child of a release-branch, use the highest branch or tag.
+  foreach (RELEASE_BRANCH ${RELEASE_BRANCHES})
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} merge-base --is-ancestor ${RELEASE_BRANCH} HEAD
+      RESULT_VARIABLE RESULT
+    )
+    if ("${RESULT}" EQUAL 0)
+      # This branch is a child branch of a release-branch.
+      string(REGEX MATCH "/release-v([0-9]+)\\.([0-9]+)" IGNORED "${RELEASE_BRANCH}")
+      set(branch_major ${CMAKE_MATCH_1})
+      set(branch_minor ${CMAKE_MATCH_2})
+      set(ON_RELEASE_BRANCH 1)
+      break()
+    endif()
+  endforeach()
+  # branch_major and branch_minor are now either the highest version, or the
+  # one that is a parent of this branch.
+  if ("${branch_major}.${branch_minor}" VERSION_GREATER "${major}.${minor}")
+    set(major ${branch_major})
+    set(minor ${branch_minor})
+    set(patch 0)
+  endif()
+  if ("${ON_RELEASE_BRANCH}")
+    # We are a child of a release branch.
+    # Just increment the patch number.
+    MATH(EXPR patch "${patch}+1")
+  else()
+    MATH(EXPR minor "${minor}+1")
+    set(patch 0)
+  endif()
   # Semver requires the dot-separated identifiers to comprise only alphanumerics and hyphens.
   string(REGEX REPLACE "[^.0-9A-Za-z-]" "-" SANITIZED_BRANCH ${CURRENT_BRANCH})
-  MATH(EXPR minor "${minor}+1")
-  set(${VERSION} "v${major}.${minor}.0-pre.${CURRENT_COMMIT_NO}+${SANITIZED_BRANCH}.${CURRENT_COMMIT_SHORT}" PARENT_SCOPE)
+  set(${VERSION} "v${major}.${minor}.${patch}-pre.${CURRENT_COMMIT_NO}+${SANITIZED_BRANCH}.${CURRENT_COMMIT_SHORT}" PARENT_SCOPE)
 endfunction()
 
 # Print the git-version on stdout:
