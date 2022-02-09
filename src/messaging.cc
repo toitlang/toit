328781a2caf938ec35f3562f4d7f68444a80c4b7
Kasper Lund <kasper@toit.io>
2022-02-09 07:18:48 +0100

    Add support for external messaging

--- src/messaging.cc
+++ src/messaging.cc
@@ -17,6 +17,8 @@
 
 #include "objects.h"
 #include "process.h"
+#include "scheduler.h"
+#include "vm.h"
 
 namespace toit {
 
@@ -55,12 +57,7 @@ class NestingTracker {
 MessageEncoder::MessageEncoder(Process* process, uint8* buffer)
     : _process(process)
     , _program(process ? process->program() : null)
-    , _buffer(buffer)
-    , _cursor(0)
-    , _nesting(0)
-    , _malloc_failed(false)
-    , _copied_count(0)
-    , _externals_count(0) {
+    , _buffer(buffer) {
 }
 
 void MessageEncoder::encode_termination_message(uint8* buffer, uint8 value) {
@@ -182,6 +179,20 @@ bool MessageEncoder::encode_byte_array(ByteArray* object) {
   return true;
 }
 
+bool MessageEncoder::encode_byte_array_external(void* data, int length) {
+  write_uint8(TAG_BYTE_ARRAY);
+  write_cardinal(length);
+  write_pointer(data);
+  if (!encoding_for_size()) {
+    if (_copied_count >= ARRAY_SIZE(_copied)) {
+      // TODO(kasper): Report meaningful error.
+      return false;
+    }
+    _copied[_copied_count++] = data;
+  }
+  return true;
+}
+
 bool MessageEncoder::encode_copy(Object* object, int tag) {
   ASSERT(tag == TAG_STRING || tag == TAG_BYTE_ARRAY);
   ASSERT(TAG_STRING_INLINE == TAG_STRING + 1);
@@ -251,10 +262,7 @@ void MessageEncoder::write_uint64(uint64 value) {
 MessageDecoder::MessageDecoder(Process* process, uint8* buffer)
     : _process(process)
     , _program(process ? process->program() : null)
-    , _buffer(buffer)
-    , _cursor(0)
-    , _allocation_failed(false)
-    , _externals_count(0) {
+    , _buffer(buffer) {
 }
 
 bool MessageDecoder::decode_termination_message(uint8* buffer, int* value) {
@@ -386,6 +394,23 @@ Object* MessageDecoder::decode_byte_array(bool inlined) {
   return result;
 }
 
+bool MessageDecoder::decode_byte_array_external(void** data, int* length) {
+  int tag = read_uint8();
+  int encoded_length = read_cardinal();
+  if (tag == TAG_BYTE_ARRAY) {
+    *data = read_pointer();
+  } else if (tag == TAG_BYTE_ARRAY_INLINE) {
+    void* copy = malloc(encoded_length);
+    // TODO(kasper): Handle failure here.
+    memcpy(copy, &_buffer[_cursor], encoded_length);
+    *data = copy;
+  } else {
+    return false;
+  }
+  *length = encoded_length;
+  return true;
+}
+
 Object* MessageDecoder::decode_double() {
   double value = bit_cast<double>(read_uint64());
   Double* result = _process->object_heap()->allocate_double(value);
@@ -431,6 +456,57 @@ uint64 MessageDecoder::read_uint64() {
   memcpy(&result, &_buffer[_cursor], sizeof(uint64));
   _cursor += WORD_SIZE;
   return result;
+}
+
+void ExternalSystemMessageHandler::start() {
+  ASSERT(_process == null);
+  _process = _vm->scheduler()->run_external(this);
+}
+
+bool ExternalSystemMessageHandler::send(int pid, int type, void* data, int length) {
+  int buffer_size = 0;
+  { MessageEncoder encoder(null);
+    encoder.encode_byte_array_external(data, length);
+    buffer_size = encoder.size();
+  }
+
+  uint8* buffer = unvoid_cast<uint8*>(malloc(buffer_size));
+  if (buffer == null) return false;
+  MessageEncoder encoder(buffer);
+  encoder.encode_byte_array_external(data, length);
+
+  SystemMessage* message = _new SystemMessage(type, _process->group()->id(), _process->id(),
+      buffer, buffer_size);
+  if (message == null) {
+    encoder.free_copied();
+    free(buffer);
+    return false;
+  }
+  scheduler_err_t result = _vm->scheduler()->send_message(pid, message);
+  if (result == MESSAGE_OK) return true;
+  encoder.free_copied();
+  delete message;
+  return false;
+}
+
+Interpreter::Result ExternalSystemMessageHandler::run() {
+  while (true) {
+    Message* message = _process->peek_message();
+    if (message == null) {
+      return Interpreter::Result(Interpreter::Result::YIELDED);
+    }
+    if (message->is_system()) {
+      SystemMessage* system = static_cast<SystemMessage*>(message);
+      MessageDecoder decoder(system->data());
+      void* data = null;
+      int length = 0;
+      bool success = decoder.decode_byte_array_external(&data, &length);
+      int pid = system->pid();
+      int type = system->type();
+      _process->remove_first_message();
+      if (success) on_message(pid, type, data, length);
+    }
+  }
 }
 
 }  // namespace toit
