e239df98374ff1d8ae4ce4e6ad44d83199f9a710
Florian Loitsch <florian@toit.io>
2022-03-25 14:37:51 +0100

    Test the debug build on the buildbots (#600)

---
+++ toolchains/host.cmake
@@ -0,0 +1,22 @@
+# Copyright (C) 2022 Toitware ApS.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; version
+# 2.1 only.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# The license can be found in the file `LICENSE` in the top level
+# directory of this repository.
+
+set(TOIT_SYSTEM_NAME "${CMAKE_SYSTEM_NAME}")
+
+set(CMAKE_C_FLAGS_DEBUG "-Og -g" CACHE STRING "c Debug flags")
+set(CMAKE_CXX_FLAGS_DEBUG "-Og -ggdb3 -fdiagnostics-color $ENV{LOCAL_CXXFLAGS}" CACHE STRING "c++ Debug flags")
+set(CMAKE_CXX_FLAGS_RELEASE "$ENV{LOCAL_CXXFLAGS}" CACHE STRING "c++ Release flags")
+
+enable_testing()
