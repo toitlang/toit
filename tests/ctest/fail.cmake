d9784dd3df3b8353236ce6aabb75bde3777d4dff
Kasper Lund <kasper@toit.io>
2022-04-23 07:51:28 +0200

    Improve exception for not-found services (#672)
    * Improve exception for not-found services
    
    * Skip crashing external messaging test on macOS

--- tests/ctest/fail.cmake
+++ tests/ctest/fail.cmake
@@ -27,5 +27,8 @@ if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Windows" OR "${CMAKE_SYSTEM_NAME}" STREQUAL
   )
 endif()
 
-list(APPEND TOIT_SKIP_TESTS
-)
+if ("${CMAKE_SYSTEM_NAME}" MATCHES "Darwin")
+  list(APPEND TOIT_SKIP_TESTS
+    tests/ctest/external_messaging_test.cc
+  )
+endif()
