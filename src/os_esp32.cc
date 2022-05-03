fc69c58d89ab6b5c950180257f4d2f42f54bb138
Erik Corry <erik@toitware.com>
2022-05-03 16:17:43 +0200

    Wrong argument order for heap_caps_aligned_alloc

--- src/os_esp32.cc
+++ src/os_esp32.cc
@@ -350,7 +350,7 @@ void OS::free_block(Block* block) {
 void* OS::allocate_pages(uword size) {
   size = Utils::round_up(size, TOIT_PAGE_SIZE);
   HeapTagScope scope(ITERATE_CUSTOM_TAGS + TOIT_HEAP_MALLOC_TAG);
-  void* allocation = heap_caps_aligned_alloc(size, TOIT_PAGE_SIZE, MALLOC_CAP_8BIT | MALLOC_CAP_DEFAULT);
+  void* allocation = heap_caps_aligned_alloc(TOIT_PAGE_SIZE, size, MALLOC_CAP_8BIT | MALLOC_CAP_DEFAULT);
   return allocation;
 }
 
