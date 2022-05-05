cb8038de519418e81ed7eb6bf617b7817f794a8c
Erik Corry <erik@toitware.com>
2022-05-05 08:39:40 +0200

    feedback

--- src/third_party/dartino/object_memory_mark_sweep.cc
+++ src/third_party/dartino/object_memory_mark_sweep.cc
@@ -404,15 +404,13 @@ void OldSpace::clear_free_list() { free_list_.clear(); }
 void OldSpace::mark_chunk_ends_free() {
   chunk_list_.delete_wherever([&](Chunk* chunk) -> bool {
     uword top = chunk->compaction_top();
-    bool empty = top == chunk->start();
-    if (!empty) {
-      uword end = chunk->usable_end();
-      if (top != end) free_list_.add_region(top, end - top);
-      top = Utils::round_up(top, GcMetadata::CARD_SIZE);
-      GcMetadata::initialize_starts_for_chunk(chunk, top);
-      GcMetadata::initialize_remembered_set_for_chunk(chunk, top);
-    }
-    return empty;  // Remove empty chunks from list.
+    if (top == chunk->start()) return true;  // Remove empty chunks from list.
+    uword end = chunk->usable_end();
+    if (top != end) free_list_.add_region(top, end - top);
+    top = Utils::round_up(top, GcMetadata::CARD_SIZE);
+    GcMetadata::initialize_starts_for_chunk(chunk, top);
+    GcMetadata::initialize_remembered_set_for_chunk(chunk, top);
+    return false;
   });
 }
 
