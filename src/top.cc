6a21524cb0e89091f62c752681b381494b428e64
Erik Corry <erik@toit.io>
2022-06-23 09:41:15 +0200

    Disable spurious use-after-free warning (#810)
    * Disable spurious use-after-free warning
    
    * Remove work-around that doesn't work around
    
    * Remove inadvertent change

--- src/top.cc
+++ src/top.cc
@@ -71,12 +71,15 @@ void* tracing_malloc(size_t size, const char* file, int line) {
 
 #undef realloc
 void* tracing_realloc(void* ptr, size_t size, const char* file, int line) {
-  // Store the old pointer in a word, so that the compiler doesn't detect a
-  // use-after-free.
-  word old_ptr = reinterpret_cast<word>(ptr);
   void* result = realloc(ptr, size);
   if (Flags::cheap) {
-    printf("%s:%d: realloc [%p] %zd [%p]\n", file, line, reinterpret_cast<void*>(old_ptr), size, result);
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wuse-after-free"
+    // Gcc is too smart and spots the old pointer being passed to printf here,
+    // even if you temporarily cast it to an integer.  But it is also dumb and
+    // doesn't realize that a %p doesn't dereference the pointer.
+    printf("%s:%d: realloc [%p] %zd [%p]\n", file, line, ptr, size, result);
+    #pragma GCC diagnostic pop
   }
   return result;
 }
