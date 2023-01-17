148441ae0403c35f1a2c20ef59368f586aa98185
Erik Corry <erik@toit.io>
2023-01-17 16:24:53 +0100

    Fix an incorrect assert about the heap metadata.

--- src/third_party/dartino/gc_metadata.cc
+++ src/third_party/dartino/gc_metadata.cc
@@ -281,7 +281,7 @@ void GcMetadata::slow_mark(HeapObject* object, uword size) {
 }
 
 void GcMetadata::mark_stack_overflow(HeapObject* object) {
-  uword address = object->_raw();
+  uword address = reinterpret_cast<uword>(object);
   uint8* overflow_bits = overflow_bits_for(address);
   *overflow_bits |= 1U << ((address >> CARD_SIZE_LOG_2) & 7);
   // We can have a mark stack overflow in new-space where we do not normally
