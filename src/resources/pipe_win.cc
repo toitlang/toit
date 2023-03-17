2d4b90fbe5530fee19d2b5d7ba2d3ac4e958af6c
Erik Corry <erik@toit.io>
2023-03-17 12:46:26 +0100

    Fix

--- src/resources/pipe_win.cc
+++ src/resources/pipe_win.cc
@@ -511,10 +511,10 @@ static Object* fork_helper(
   if (GetFileType(startup_info.hStdError) == FILE_TYPE_PIPE && !is_inherited(err_object))
     CloseHandle(startup_info.hStdError);
 
-  if (!startup_info.hProcess) {
+  if (!process_information.hProcess) {
     // We are running on Wine, and we have started a Linux executable,
     // which means we can't track when it terminates.  But we already
-    // started the process.  I don't want to define yet another C++-thrown
+    // started the process.  We don't want to define yet another C++-thrown
     // exception for this marginal case., so we throw one of the standard
     // exceptions here, but also print a warning on stderr.
     fprintf(stderr, "Error: Running a Linux executable from Wine is not supported: '%ls'\n", command_line);
