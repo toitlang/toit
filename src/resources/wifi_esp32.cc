e7223a2ed897936d02dbca7d5dc57d5122b83b90
Erik Corry <erik@toit.io>
2023-07-26 11:29:24 +0200

    Use a better IP address for the captive portal (#1712)

--- src/resources/wifi_esp32.cc
+++ src/resources/wifi_esp32.cc
@@ -384,7 +384,17 @@ PRIMITIVE(init) {
   // because they do not correctly check for malloc failure.
   esp_netif_t* netif = null;
   if (ap) {
+    // We use this static IP for the access point because it causes
+    // Samsung phones to pop up the captive portal login page.
+    // TODO: Make this configurable.
+    esp_netif_ip_info_t two_hundred_network;
+    two_hundred_network.ip.addr = ESP_IP4TOADDR( 200, 200, 200, 1);
+    two_hundred_network.gw.addr = ESP_IP4TOADDR( 200, 200, 200, 1);
+    two_hundred_network.netmask.addr = ESP_IP4TOADDR( 255, 255, 255, 0);
+    esp_netif_inherent_config_t netif_config = ESP_NETIF_INHERENT_DEFAULT_WIFI_AP();
+    netif_config.ip_info = &two_hundred_network;
     esp_netif_config_t netif_ap_config = ESP_NETIF_DEFAULT_WIFI_AP();
+    netif_ap_config.base = &netif_config;
     netif = esp_netif_new(&netif_ap_config);
   } else {
     esp_netif_config_t netif_sta_config = ESP_NETIF_DEFAULT_WIFI_STA();
