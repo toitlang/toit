93f2ee3899284260dde9b01e192a78e3f98a4393
Florian Loitsch <florian@toit.io>
2023-10-04 11:30:11 +0200

    Don't allow mutations of final fields. (#1844)
    Once the 'super' has been called we must not allow changes to final
    fields.
    
    Fixes https://github.com/toitware/toit/issues/4178

--- src/compiler/resolver_method.h
+++ src/compiler/resolver_method.h
@@ -70,6 +70,7 @@ class MethodResolver : public ast::Visitor {
       , scope_(scope)
       , resolution_mode_(STATIC)
       , super_forcing_expression_(null)
+      , block_may_mutate_final_(true)
       , current_lambda_(null)
       , loop_status_(NO_LOOP)
       , loop_block_depth_(0) {}
@@ -108,6 +109,21 @@ class MethodResolver : public ast::Visitor {
   ResolutionMode resolution_mode_;
   // The expression that forced to switch the constructor to instance mode.
   ast::Expression* super_forcing_expression_;
+  /// Whether the current block is allowed to mutate a final field, assuming
+  ///   we are in the static part of a constructor.
+  /// Specifically, we do allow:
+  ///     class A:
+  ///       field_x/int
+  ///       constructor:
+  ///         [1].do: field_x = it
+  ///
+  /// However, we don't allow:
+  ///     constructor:
+  ///        b := : field_x = 499
+  ///        x = 42
+  ///        super
+  ///        b.call
+  bool block_may_mutate_final_;
   ast::Node* current_lambda_;
   LoopStatus loop_status_;
   int loop_block_depth_;
@@ -265,6 +281,7 @@ class MethodResolver : public ast::Visitor {
                          Symbol label);
   ir::Code* _create_block(ast::Block* node,
                           bool has_implicit_it_parameter,
+                          bool may_mutate_final,
                           Symbol label);
   ir::Expression* _create_lambda(ast::Lambda* node,
                                  Symbol label);
