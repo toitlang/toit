39d4003f16778177df36b24e9c8f7eb433fdf52d
Florian Loitsch <florian@toit.io>
2023-10-06 19:28:24 +0200

    Update queryables to include mixins.
    Queryables are used to check whether a method has a specific method.

--- src/compiler/queryable_class.cc
+++ src/compiler/queryable_class.cc
@@ -66,47 +66,64 @@ UnorderedMap<Class*, QueryableClass> build_queryables_from_resolution_shapes(Pro
 
   UnorderedMap<Class*, QueryableClass> result;
 
-  for (auto klass : program->classes()) {
-    QueryableClass::SelectorMap methods;
+  for (int i = 0; i < 2; i++) {
+    // We run in two phases:
+    // The first phase only does mixins.
+    // The second the rest.
+    for (auto klass : program->classes()) {
+      if (i == 0 && !klass->is_mixin()) continue;
+      if (i == 1 && klass->is_mixin()) continue;
 
-    if (klass->super() != null) {
-      // We know we already dealt with the super, because the classes are sorted
-      //   by inheritance.
-      // Insert the superclass methods first, and then overwrite the ones that this class has.
-      methods.add_all(result[klass->super()].methods());
-    } else if (klass != object_class) {
-      // Interface or Mixin.
-      // Add the Object methods as they have to be available for every object.
-      methods.add_all(result[object_class].methods());
-    }
+      QueryableClass::SelectorMap methods;
 
-    for (auto method : klass->methods()) {
-      auto name = method->name();
-      auto method_shape = method->resolution_shape();
-      auto plain_shape = method_shape.to_plain_shape();
-
-      if (!method_shape.has_optional_parameters()) {
-        // No need to see which versions of the method-shape are needed.
-        Selector<PlainShape> selector(name, plain_shape);
-        methods[selector] = method;
-        continue;
+      if (klass->super() != null) {
+        // We know we already dealt with the super, because the classes are sorted
+        //   by inheritance.
+        // Insert the superclass methods first, and then overwrite the ones that this class has.
+        ASSERT(result.find(klass->super()) != result.end());
+        methods.add_all(result[klass->super()].methods());
+      } else if (klass != object_class) {
+        // Interface or Mixin.
+        // Add the Object methods as they have to be available for every object.
+        methods.add_all(result[object_class].methods());
       }
 
-      auto probe = invoked_selectors.find(name);
-      if (probe == invoked_selectors.end()) {
-        // Not called at all. We can just ignore it.
-        continue;
+      for (auto mixin : klass->mixins()) {
+        // We do mixins first, and also have sorted mixins in such a way
+        // that their "parents" are always first.
+        // The mixin must thus already be handled.
+        ASSERT(result.find(mixin) != result.end());
+        methods.add_all(result[mixin].methods());
       }
 
-      for (auto call_shape : probe->second) {
-        if (method_shape.accepts(call_shape)) {
-          Selector<PlainShape> selector(name, call_shape.to_plain_shape());
+      for (auto method : klass->methods()) {
+        auto name = method->name();
+        auto method_shape = method->resolution_shape();
+        auto plain_shape = method_shape.to_plain_shape();
+
+        if (!method_shape.has_optional_parameters()) {
+          // No need to see which versions of the method-shape are needed.
+          Selector<PlainShape> selector(name, plain_shape);
           methods[selector] = method;
+          continue;
+        }
+
+        auto probe = invoked_selectors.find(name);
+        if (probe == invoked_selectors.end()) {
+          // Not called at all. We can just ignore it.
+          continue;
+        }
+
+        for (auto call_shape : probe->second) {
+          if (method_shape.accepts(call_shape)) {
+            Selector<PlainShape> selector(name, call_shape.to_plain_shape());
+            methods[selector] = method;
+          }
         }
       }
-    }
 
-    result[klass] = QueryableClass(klass, methods);
+      result[klass] = QueryableClass(klass, methods);
+    }
   }
   return result;
 }
