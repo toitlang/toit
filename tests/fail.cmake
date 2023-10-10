05236c8dd64732a6073168210b1eafd327c8679e
Florian Loitsch <florian@toit.io>
2023-10-10 11:32:05 +0200

    Add support for flaky tests.

--- tests/fail.cmake
+++ tests/fail.cmake
@@ -24,6 +24,10 @@ endif()
 list(APPEND TOIT_SKIP_TESTS
 )
 
+list(APPEND TOIT_FLAKY_TESTS
+  tests/tls-global-cert-test-slow.toit
+)
+
 if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Windows" OR "${CMAKE_SYSTEM_NAME}" STREQUAL "MSYS")
   list(APPEND TOIT_FAILING_TESTS
     tests/time-test.toit # https://github.com/toitlang/toit/issues/1369
