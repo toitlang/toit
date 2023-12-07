7bcbc484fe83a48f6f04e5e899d862f03ed4805c
Florian Loitsch <florian@toit.io>
2023-12-07 12:54:37 +0100

    Fix Windows signing. (#1986)
    Co-authored-by: Kasper Lund <kasper@toit.io>

--- tools/windows_installer/build.bat
+++ tools/windows_installer/build.bat
@@ -4,7 +4,7 @@ set BUILD_DIRECTORY=%3
 
 set INSTALLER_NAME="%cd%\tools\windows_installer\toit_installer.exe"
 
-set inno="C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
+set INNO=ISCC.exe
 
-%inno% /Qp /dMyAppVersion=%VERSION% /dSdkPath="%SDK_PATH%" "%cd%/tools/windows_installer/installer.iss"
+%INNO% /Qp /dMyAppVersion=%VERSION% /dSdkPath="%SDK_PATH%" "%cd%/tools/windows_installer/installer.iss"
 move %INSTALLER_NAME% %BUILD_DIRECTORY%
