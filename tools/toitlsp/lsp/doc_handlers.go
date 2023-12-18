a65775f8127e6b74ee263f78d9ab56bb8f21e192
Florian Loitsch <florian@toit.io>
2023-12-18 17:33:47 +0400

    Fix typo.

--- tools/toitlsp/lsp/doc_handlers.go
+++ tools/toitlsp/lsp/doc_handlers.go
@@ -39,7 +39,7 @@ const (
 func isInsideDotPackages(uri lsp.DocumentURI) bool {
 	return strings.Contains(string(uri), "/.packages/") ||
 		strings.Contains(string(uri), "%5C.packages%5C") ||
-		strings.Contains(string(uri), "%5c.packages%5c") ||
+		strings.Contains(string(uri), "%5c.packages%5c")
 }
 
 func (s *Server) TextDocumentDidOpen(ctx context.Context, conn *jsonrpc2.Conn, req lsp.DidOpenTextDocumentParams) error {
