8dce6d7286f9106fd449fb96b028aa50dfa80761
Erik Corry <erik@toit.io>
2024-02-14 16:00:58 +0100

    const compile

--- src/resources/tls.cc
+++ src/resources/tls.cc
@@ -131,7 +131,7 @@ static void tagging_mbedtls_free(void* address) {
 }
 
 #ifdef TOIT_WINDOWS
-static bool get_roots_from_store(HCERTSTORE store, const char* issuer, mbedtls_x509_crt*** last) {
+static bool get_roots_from_store(const HCERTSTORE store, const char* issuer, mbedtls_x509_crt*** last) {
   if (!store) return false;
   CERT_CONTEXT* cert_context = CertEnumCertificatesInStore(store, null);
   while (cert_context) {
@@ -143,7 +143,7 @@ static bool get_roots_from_store(HCERTSTORE store, const char* issuer, mbedtls_x
 }
 
 static bool find_matching_roots_windows(const uint8* issuer_buffer, mbedtls_x509_crt*** last) {
-  const char issuer = char_cast(issuer_buffer);
+  const char* issuer = char_cast(issuer_buffer);
   HCERTSTORE root_store = CertOpenStore(CERT_STORE_PROC_SYSTEM, 0, null, CERT_SYSTEM_STORE_CURRENT_USER, L"ROOT");
   bool root_found = get_roots_from_store(root_store, issuer, last);
   HCERTSTORE ca_store = CertOpenStore(CERT_STORE_PROC_SYSTEM, 0, null, CERT_SYSTEM_STORE_CURRENT_USER, L"CA");
