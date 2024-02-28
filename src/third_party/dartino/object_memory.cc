5f40ff0aa82072fa052deb12bbacbf4a0483f7ce
Erik Corry <erik@toit.io>
2024-02-28 09:35:21 +0100

    Simplify page allocation. (#2108)
    Co-authored-by: Kasper Lund <kasper@toit.io>

--- src/third_party/dartino/object_memory.cc
+++ src/third_party/dartino/object_memory.cc
@@ -231,25 +231,10 @@ void Chunk::find(uword word, const char* name) {
 #endif
 
 Chunk* ObjectMemory::allocate_chunk(Space* owner, uword size) {
-  static const int UNUSABLE_SIZE = 50;
-  void* unusable_pages[UNUSABLE_SIZE];
-  size = Utils::round_up(size, TOIT_PAGE_SIZE);
-  uword lowest = GcMetadata::lowest_old_space_address();
-  for (int i = 0; i < UNUSABLE_SIZE; i++) {
-    void* memory = OS::allocate_pages(size);
-    if (memory == null ||
-        (GcMetadata::in_metadata_range(memory) &&
-         GcMetadata::in_metadata_range(reinterpret_cast<uword>(memory) + size - 1))) {
-      for (int j = 0; j < i; j++) OS::free_pages(unusable_pages[j], size);
-      return allocate_chunk_helper(owner, size, memory);
-    }
-    unusable_pages[i] = memory;
-  }
-  printf("New allocation %p-%p\n", unusable_pages[0], unvoid_cast<char*>(unusable_pages[0]) + size);
-  printf("Metadata range %p-%p\n", reinterpret_cast<void*>(lowest), reinterpret_cast<uint8*>(lowest) + GcMetadata::heap_extent());
-  FATAL("Toit heap outside expected range");
+  void* memory = OS::allocate_pages(Utils::round_up(size, TOIT_PAGE_SIZE));
+  if (!memory) return null;
+  return allocate_chunk_helper(owner, size, memory);
 }
-
 
 Chunk* ObjectMemory::allocate_chunk_helper(Space* owner, uword size, void* memory) {
   if (memory == null) return null;
