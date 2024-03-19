62123cbf8c86676e0b4edd5ab1d253a4092f59eb
Erik Corry <erik@toit.io>
2024-03-19 13:55:05 +0100

    Revert inadvertent whitespace

--- src/third_party/dartino/object_memory.cc
+++ src/third_party/dartino/object_memory.cc
@@ -307,7 +307,6 @@ void ObjectMemory::set_up() {
   GcMetadata::set_up();
   spare_chunk_ = allocate_chunk(null, TOIT_PAGE_SIZE);
   if (!spare_chunk_) FATAL("Can't allocate initial spare chunk");
-
   if (spare_chunk_mutex_) FATAL("Can't call ObjectMemory::set_up twice");
   spare_chunk_mutex_ = OS::allocate_mutex(7, "Spare memory chunk");
   if (!spare_chunk_mutex_) FATAL("Can't allocate spare memory mutex");
