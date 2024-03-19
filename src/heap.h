5c6b572b5e76966980f010c2a07368603d30e1e8
Erik Corry <erik@toit.io>
2024-03-19 15:43:04 +0100

    Faster allocation of new instance objects. (#2154)
    Instance creation in a tight loop - two field objects:
    
    Before:
    
    ```
    $ time toit.run it.snap
    real	0m1.339s
    user	0m1.272s
    sys	0m0.067s
    ```
    
    After:
    
    ```
    $ time toit.run it.snap
    real	0m0.834s
    user	0m0.770s
    sys	0m0.061s
    ```
    
    ByteArray slice creation in a tight loop:
    
    Before:
    
    ```
    $ time toit.run st.snap
    real	0m1.342s
    user	0m1.311s
    sys	0m0.031s
    ```
    
    After:
    
    ```
    $ time toit.run st.snap
    real	0m1.190s
    user	0m1.139s
    sys	0m0.041s
    ```

--- src/heap.h
+++ src/heap.h
@@ -64,8 +64,8 @@ class ObjectHeap {
   inline bool cross_process_gc_needed() const { return two_space_heap_.cross_process_gc_needed(); }
 
   // Shared allocation operations.
+  // Allocates an instance object in new space and fills it with nulls.
   Instance* allocate_instance(Smi* class_id);
-  Instance* allocate_instance(TypeTag class_tag, Smi* class_id, Smi* instance_size);
   Array* allocate_array(int length, Object* filler);
   ByteArray* allocate_external_byte_array(int length, uint8* memory, bool dispose, bool clear_content = true);
   String* allocate_external_string(int length, uint8* memory, bool dispose);
