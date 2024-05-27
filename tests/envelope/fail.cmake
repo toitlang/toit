b1081028ce878f9d84cfb589a7d4bbd7394a982a
Florian Loitsch <florian@toit.io>
2024-05-27 10:10:20 +0200

    Enable tests again.

--- tests/envelope/fail.cmake
+++ tests/envelope/fail.cmake
@@ -12,13 +12,3 @@
 #
 # The license can be found in the file `LICENSE` in the top level
 # directory of this repository.
-
-# Doesn't work on 32 bits yet.
-if (NOT CMAKE_SIZEOF_VOID_P EQUAL 8)
-  set(TOIT_SKIP_TESTS
-    tests/envelope/boot-hello-test.toit
-    tests/envelope/boot-upgrade-test.toit
-    tests/envelope/firmware-upgrade-test.toit
-    tests/envelope/hello-test.toit
-  )
-endif()
