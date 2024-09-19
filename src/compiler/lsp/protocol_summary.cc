b1a404e5b84ac29e509121d097da69ead0a9ee61
Florian Loitsch <florian@toit.io>
2024-09-19 12:35:11 +0200

    Prefix operators with "operator" in the summary. (#2462)
    This also means that they are prefixed with operator in the toitdocs.

--- src/compiler/lsp/protocol_summary.cc
+++ src/compiler/lsp/protocol_summary.cc
@@ -23,6 +23,7 @@
 #include "../ir.h"
 #include "../map.h"
 #include "../resolver_scope.h"
+#include "../scanner.h"  // For "is_identifier_start"
 #include "../sources.h"
 #include "../toitdoc.h"
 #include "../toitdoc_node.h"
@@ -56,6 +57,10 @@ struct ToitdocPath {
 template<typename T> static int length_of(const std::vector<T>& v) { return static_cast<int>(v.size()); }
 template<typename T> static int length_of(const Set<T>& v) { return v.size(); }
 template<typename T> static int length_of(const List<T>& v) { return v.length(); }
+
+static bool is_operator_name(const char* name) {
+  return !IdentifierValidator::is_identifier_start(name[0]);
+}
 
 class ToitdocWriter : public toitdoc::Visitor {
  public:
@@ -173,7 +178,11 @@ class ToitdocWriter : public toitdoc::Visitor {
       this->printf("%d\n", kind_id);
       this->printf("%s\n", path.module->unit()->absolute_path());
       if (holder_name.is_valid()) print_symbol(holder_name);
-      print_symbol(name);
+      if (name.is_valid() && is_operator_name(name.c_str())) {
+        print_symbol(name, "operator ");
+      } else {
+        print_symbol(name);
+      }
       if (shape.is_valid()) {
         print_shape(shape);
       }
@@ -195,12 +204,13 @@ class ToitdocWriter : public toitdoc::Visitor {
     for (auto element : elements) { (this->*callback)(element); }
   }
 
-  void print_symbol(Symbol symbol) {
+  void print_symbol(Symbol symbol, const char* prefix = "") {
     if (!symbol.is_valid()) {
       this->printf("0\n\n");
     } else {
       const char* str = symbol.c_str();
-      this->printf("%zd\n%s\n", strlen(str), str);
+      size_t length = strlen(prefix) + strlen(str);
+      this->printf("%zd\n%s%s\n", length, prefix, str);
     }
   }
 
@@ -460,8 +470,11 @@ void Writer::print_field(ir::Field* field) {
 
 void Writer::print_method(ir::Method* method) {
   if (method->name().is_valid()) {
+    const char* name = method->name().c_str();
     if (method->is_setter()) {
-      this->printf_external("%s=\n", method->name().c_str());
+      this->printf_external("%s=\n", name);
+    } else if (is_operator_name(name)) {
+      this->printf_external("operator %s\n", name);
     } else {
       this->printf_external("%s\n", method->name().c_str());
     }
