670aa53e6a0ca5fedc0d136f9d017351d5bfb7c4
Florian Loitsch <florian@toit.io>
2025-03-13 16:13:07 +0100

    Rewrite pulse-counter driver. (#2738)

--- src/primitive.h
+++ src/primitive.h
@@ -486,7 +486,6 @@ namespace toit {
   PRIMITIVE(new_unit, 4)                     \
   PRIMITIVE(close_unit, 1)                   \
   PRIMITIVE(new_channel, 7)                  \
-  PRIMITIVE(close_channel, 2)                \
   PRIMITIVE(start, 1)                        \
   PRIMITIVE(stop, 1)                         \
   PRIMITIVE(clear, 1)                        \
