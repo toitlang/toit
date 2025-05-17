1e4426ab508f9f30db9fa8ad2c3a17712f5e1274
Florian Loitsch <florian@loitsch.com>
2025-05-17 10:41:55 +0200

    Use tab for the dependency file.

--- src/compiler/dep_writer.cc
+++ src/compiler/dep_writer.cc
@@ -105,7 +105,7 @@ void PlainDepWriter::generate_dependency_entry(const char* source, List<const ch
   write(source);
   write(":\n");
   for (auto dep : dependencies) {
-    write("  ");
+    write("\t");
     writeln(dep);
   }
 }
