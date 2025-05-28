de7dfd685f499b1408f5ca20d4fe075f55b92001
Florian Loitsch <florian@toit.io>
2025-05-28 13:50:28 +0200

    Add a codepage test. (#2856)

---
+++ tests/test-cp874.cmd
@@ -0,0 +1,6 @@
+REM This script has been saved with 874 (Thai) encoding.
+REM It tests that the Toit tools can handle non-UTF8 filenames.
+chcp 874
+mkdir à´Ê¡ì·çÍ»
+echo main: print "hello" > à´Ê¡ì·çÍ»\main.toit
+build\host\sdk\bin\toit.exe à´Ê¡ì·çÍ»\main.toit > output.txt
