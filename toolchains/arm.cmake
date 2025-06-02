3d40c4e7b9ccc3f8ae49a68708010e965959f939
Florian Loitsch <florian@toit.io>
2025-06-02 15:48:55 +0200

    Feedback.

--- toolchains/arm.cmake
+++ toolchains/arm.cmake
@@ -66,6 +66,7 @@ set(CMAKE_CXX_COMPILER_TARGET "${ARM_TARGET}" CACHE STRING "c++ compiler target 
 
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -x assembler-with-cpp" CACHE STRING "asm flags")
 
+# Force the linker flags in case they were already set. The '-fuse-ld=lld' flag is critical.
 set(CMAKE_EXE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT} -fuse-ld=lld"
   CACHE STRING "The linker flags for the toolchain" FORCE)
 
