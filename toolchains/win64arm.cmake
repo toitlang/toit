f19adcc690e9fdf6a175e07903ec341837dccf0d
Florian Loitsch <florian@toit.io>
2025-06-02 10:33:56 +0200

    Fix manifest support. (#2860)

--- toolchains/win64arm.cmake
+++ toolchains/win64arm.cmake
@@ -15,16 +15,16 @@
 
 # Note: mbedtls fails unless _MSC_VER is replaced with _WIN32 in line 198 of timimg.c
 
-set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_NAME Windows CACHE STRING "The system name for the toolchain" FORCE)
 
 set(triple armv7-w64-mingw32)
-set(CMAKE_COMPILER_IS_CLANG 1)
-set(_MSC_VER 1)
+set(CMAKE_COMPILER_IS_CLANG 1 CACHE BOOL "Set to 1 if the compiler is clang" FORCE)
+set(_MSC_VER 1 CACHE STRING "Set to 1 to emulate MSVC compiler version" FORCE)
 
-set(CMAKE_C_COMPILER armv7-w64-mingw32-gcc)
-set(CMAKE_C_COMPILER_TARGET ${triple})
-set(CMAKE_CXX_COMPILER armv7-w64-mingw32-g++)
-set(CMAKE_CXX_COMPILER_TARGET ${triple})
+set(CMAKE_C_COMPILER armv7-w64-mingw32-gcc CACHE STRING "The C compiler for the toolchain")
+set(CMAKE_C_COMPILER_TARGET ${triple} CACHE STRING "The target for the C compiler")
+set(CMAKE_CXX_COMPILER armv7-w64-mingw32-g++ CACHE STRING "The C++ compiler for the toolchain")
+set(CMAKE_CXX_COMPILER_TARGET ${triple} CACHE STRING "The target for the C++ compiler")
 
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -m64 -x assembler-with-cpp" CACHE STRING "asm flags")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m64 -Wno-error=sign-compare -DMBEDTLS_TIMING_ALT=1 -I${CMAKE_SOURCE_DIR}/src/ports" CACHE STRING "c flags")
@@ -36,9 +36,9 @@ set(CMAKE_C_FLAGS_RELEASE "-Os" CACHE STRING "c Release flags")
 set(CMAKE_CXX_FLAGS_DEBUG "-Og -g" CACHE STRING "c++ Debug flags")
 set(CMAKE_CXX_FLAGS_RELEASE "-Os" CACHE STRING "c++ Release flags")
 
-set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++ -static")
+set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++ -static" CACHE STRING "Linker flags for executables")
 
-set(TOIT_SYSTEM_NAME "${CMAKE_SYSTEM_NAME}")
+set(TOIT_SYSTEM_NAME "${CMAKE_SYSTEM_NAME}" CACHE STRING "The system name for the host toolchain")
 
-set(GOOS "windows")
-set(GOARCH "arm64")
+set(GOOS "windows" CACHE STRING "The GOOS for the toolchain")
+set(GOARCH "arm64" CACHE STRING "The GOARCH for the toolchain")
