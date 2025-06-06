46deb191b61a2c52107424a11c74093b84a9ae77
Florian Loitsch <florian@toit.io>
2025-06-06 19:30:39 +0200

    x.

--- tests/test-cp874.cmd
+++ tests/test-cp874.cmd
@@ -4,5 +4,5 @@ chcp 874
 mkdir à´Ê¡ì·çÍ»
 echo main: print "hello" > à´Ê¡ì·çÍ»\main.toit
 build\host\sdk\bin\toit.exe à´Ê¡ì·çÍ»\main.toit > output.txt
-build\host\sdk\bin\toit.exe compile --snapshot -o output.snapshot à´Ê¡ì·çÍ»\main.toit
-build\host\sdk\bin\toit.exe tool snapshot-to-image -o output.image --format=binary -m32 output.snapshot
+REM build\host\sdk\bin\toit.exe compile --snapshot -o output.snapshot à´Ê¡ì·çÍ»\main.toit
+REM build\host\sdk\bin\toit.exe tool snapshot-to-image -o output.image --format=binary -m32 output.snapshot
