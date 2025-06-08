656282605cc6029da717a4e864f63a1d6e72c98a
Florian Loitsch <florian@toit.io>
2025-06-08 18:03:04 +0200

    Fix bad freeing.

--- src/main_utf_8_helper.cc
+++ src/main_utf_8_helper.cc
@@ -35,24 +35,29 @@ int run_with_utf_8_args(int (*main_func)(int, char**), int argc, char** argv) {
   int wargc;
   LPWSTR* wargv = CommandLineToArgvW(cmdLineW, &wargc);
 
+  // According to the internet, argc and wargc might differ. So we use wargc from now on.
   // Convert wide char arguments to UTF-8 arguments.
-  char** utf_8_argv = toit::unvoid_cast<char**>(malloc(sizeof(char*) * (argc + 1)));
-  utf_8_argv[argc] = null;
-  for (int i = 0; i < argc; ++i) {
+  // Since the callee may change the argv, we keep a copy of the UTF-8 arguments.
+  char** utf_8_args = toit::unvoid_cast<char**>(malloc(sizeof(char*) * (wargc + 1)));
+  char** utf_8_copy = toit::unvoid_cast<char**>(malloc(sizeof(char*) * (wargc + 1)));
+  utf_8_args[wargc] = null;
+  for (int i = 0; i < wargc; ++i) {
     uword total_len = toit::Utils::utf_16_to_8(wargv[i], wcslen(wargv[i]));
     char* utf_8_arg = toit::unvoid_cast<char*>(malloc(total_len + 1));  // +1 for null terminator.
     toit::Utils::utf_16_to_8(wargv[i], wcslen(wargv[i]), reinterpret_cast<uint8*>(utf_8_arg), total_len);
     utf_8_arg[total_len] = '\0';  // Null-terminate the string.
-    utf_8_argv[i] = utf_8_arg;
+    utf_8_args[i] = utf_8_arg;  // Keep a copy.
+    argv[i] = utf_8_arg;
   }
   LocalFree(wargv);
 
-  int exit_state = main_func(argc, utf_8_argv);
+  int exit_state = main_func(wargc, utf_8_args);
   // Free the converted arguments.
-  for (int i = 0; i < argc; ++i) {
-    free(utf_8_argv[i]);
+  for (int i = 0; i < wargc; ++i) {
+    free(utf_8_copy[i]);
   }
-  free(utf_8_argv);
+  free(utf_8_args);
+  free(utf_8_copy);
   return exit_state;
 }
 
