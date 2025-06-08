d765ce9e6e79cea72259cc620bbabe44e1051ec5
Florian Loitsch <florian@toit.io>
2025-06-08 14:16:58 +0200

    Fix windows utf-8 issues.

---
+++ src/main_utf_8_helper.cc
@@ -0,0 +1,58 @@
+// Copyright (C) 2025 Toit contributors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; version
+// 2.1 only.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// The license can be found in the file `LICENSE` in the top level
+// directory of this repository.
+
+#include "main_utf_8_helper.h"
+
+#ifndef TOIT_WINDOWS
+
+// For non-Windows platforms, we can directly use the standard main function.
+int run_with_utf_8_args(int (*main_func)(int, char**), int argc, char** argv) {
+  return main_func(argc, argv);
+}
+
+#else
+
+#include <windows.h>
+#include "utils.h"
+
+int run_with_utf_8_args(int (*main_func)(int, char**), int argc, char** argv) {
+  // The argv are in local code page, so we don't use them.
+  // Instead we use the wide character command line arguments accessible via GetCommandLineW.
+  LPWSTR cmdLineW = GetCommandLineW();
+  int wargc;
+  LPWSTR* wargv = CommandLineToArgvW(cmdLineW, &wargc);
+
+  // Convert wide char arguments to UTF-8 arguments.
+  char** utf_8_argv = toit::unvoid_cast<char**>(malloc(sizeof(char*) * (argc + 1)));
+  utf_8_argv[argc] = null;
+  for (int i = 0; i < argc; ++i) {
+    uword total_len = toit::Utils::utf_16_to_8(wargv[i], wcslen(wargv[i]));
+    char* utf_8_arg = toit::unvoid_cast<char*>(malloc(total_len + 1));  // +1 for null terminator.
+    toit::Utils::utf_16_to_8(wargv[i], wcslen(wargv[i]), reinterpret_cast<uint8*>(utf_8_arg), total_len);
+    utf_8_arg[total_len] = '\0';  // Null-terminate the string.
+    utf_8_argv[i] = utf_8_arg;
+  }
+  LocalFree(wargv);
+
+  int exit_state = main_func(argc, utf_8_argv);
+  // Free the converted arguments.
+  for (int i = 0; i < argc; ++i) {
+    free(utf_8_argv[i]);
+  }
+  free(utf_8_argv);
+  return exit_state;
+}
+
+#endif  // TOIT_WINDOWS
--- tests/test-cp874.cmd
+++ tests/test-cp874.cmd
@@ -3,4 +3,7 @@ REM It tests that the Toit tools can handle non-UTF8 filenames.
 chcp 874
 mkdir à´Ê¡ì·çÍ»
 echo main: print "hello" > à´Ê¡ì·çÍ»\main.toit
+build\host\sdk\bin\toit.exe à´Ê¡ì·çÍ»\main.toit
 build\host\sdk\bin\toit.exe à´Ê¡ì·çÍ»\main.toit > output.txt
+build\host\sdk\bin\toit.exe compile --snapshot -o output.snapshot à´Ê¡ì·çÍ»\main.toit
+build\host\sdk\bin\toit.exe tool snapshot-to-image -o output.image --format=binary -m32 output.snapshot
